713e94fa06e1c5a4cc074aa371fa57964d8c8fc0
adilmannan <adilmanna077@gmail.com>
2025-08-23 20:04:21 +0530

    added video handling routes

--- lib/auth.ts
+++ lib/auth.ts
@@ -36,7 +36,7 @@ export const authOptions: NextAuthOptions = {
 					}
 
 					return{
-						id: user._id.toString();
+						id: user._id.toString(),
 						email: user.email
 					}
         } catch (error) {
@@ -71,3 +71,4 @@ export const authOptions: NextAuthOptions = {
 	},
 	secret: process.env.NEXTAUTH_SECRET
 };
+
